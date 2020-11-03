35153d323e483bc20defe1ca28a3e764c178814a
EugeniVoitehovski <33103080+EugeniVoitehovski@users.noreply.github.com>
2020-11-03 22:51:26 +0300

    Add own method of detect

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
+from langdetect import detect
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import io
+
+
+def readFromFileEnglishText():
+    try:
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+        fp = open("texts/english.pdf", 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.get_pages(fp,
+                                      check_extractable=True):
+            interpreter.process_page(page)
+        text = retstr.getvalue()
+        fp.close()
+        device.close()
+        retstr.close()
+        # print(text)
+        return text
+    except IOError:
+        print("No file")
+
+
+def readFromFileRussianText():
+    try:
+        rsrcmgr = PDFResourceManager()
+        retstr = io.StringIO()
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+        fp = open("texts/russian.pdf", 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        for page in PDFPage.get_pages(fp,
+                                      check_extractable=True):
+            interpreter.process_page(page)
+        text = retstr.getvalue()
+        fp.close()
+        device.close()
+        retstr.close()
+        #print(text)
+        return text
+    except IOError:
+        print("No file")
 
 def choose_method():
     method = int(input('Выберите метод из предложенных:\n'
@@ -18,10 +67,15 @@ def choose_method():
 def frequencyWordMethod():
     print('В разработке')
 
+
 def shortWordMethod():
     print('В разработке')
 
+
 def ownMethod():
-    print('В разработке')
+    print(detect(str(readFromFileEnglishText())))
+    print(detect(str(readFromFileRussianText())))
+
 
 choose_method()
+
